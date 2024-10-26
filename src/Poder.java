e11ac92be1d6c3dfef84c30ef6ee7b5e11f831ca
Hanna-gt <hgraciat@udistrital.edu.co>
2024-10-26 18:50:22 -0500

    se creó la clase poder y sus respectivos métodos

--- src/Poder.java
+++ src/Poder.java
@@ -1,10 +1,45 @@
 public class Poder {
     private String nombre;
+    private String descripcion;
     private int nivel;
+    private TipoPoder tipo;
 
-    public Poder(String nombre, int nivel) {
+    public Poder(String nombre, String descripcion, int nivel, TipoPoder tipo) {
         this.nombre = nombre;
+        this.descripcion = descripcion;
         this.nivel = nivel;
+        this.tipo = tipo;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public TipoPoder getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPoder tipo) {
+        this.tipo = tipo;
     }
 }
