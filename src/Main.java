2fe51a9d93fcf6d941a93a31649b27775b02f637
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-27 14:06:18 -0500

    Empezando a mirar como hacer el tema de los archivos txt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
-
+        crearArchivo("archivo1.txt");
         System.out.println("Hello world!");
-        Ventana v1= new Ventana();
-        v1.setVisible(true);
+        //Ventana v1= new Ventana();
+        //v1.setVisible(true);
+
+    }
+
+    public static void crearArchivo(String nombrea)
+    {
+        File archivo = new File(nombrea);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
