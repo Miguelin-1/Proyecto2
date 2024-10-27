2f5c25f9ee0b50682ef1fb23cbca537ef6e808b5
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-27 13:45:52 -0500

    Creada la primera ventana con un par de cositas

--- src/Ventana.java
+++ src/Ventana.java
@@ -3,9 +3,12 @@ import java.awt.*;
 
 public class Ventana extends JFrame {
 
-    @Override
-    public void setSize(int width, int height) {
-        super.setSize(500, 500);
+    public Ventana(){
+        //Este metodo lo que hace es hacer el tamaño de la ventana y la posicion por coordenadas
+        setBounds(0,0,500,500);
+        //setLocationRelativeTo(null); //este metodo pone la ventana en el centro de la pantalla
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Pendiente poner un buen titulo");
     }
+
 }
