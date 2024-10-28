c1773d8a0c13ca93749c04e942d4ad60ad81b526
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-27 20:33:07 -0500

    Se cambió el metodo escribir archivo para que se puedan escribir en mas archivos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         ModificarArchivos.crearArchivos();
-       ModificarArchivos.escribirArchivo("archivo1.txt","Hola como estás");
         //Ventana v1= new Ventana();
         //v1.setVisible(true);
 
--- src/ModificarArchivos.java
+++ src/ModificarArchivos.java
@@ -1,5 +1,6 @@
+import java.awt.*;
 import java.io.*;
-
+import java.util.List;
 public class ModificarArchivos {
     public static void crearArchivos()
     {
@@ -15,18 +16,26 @@ public class ModificarArchivos {
             }
         }
     }
-    public static void escribirArchivo(String nombrea,String contenido){
-        File archivo=new File(nombrea);
-        try {
-            PrintWriter salida = new PrintWriter(new FileWriter(archivo,true));
-            salida.print(contenido);
-            salida.close();
 
-            System.out.println("Se ha escrito en el archivo archivo");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch(IOException e){
-           e.printStackTrace(System.out);
+    public static void escribirArchivos(List<String> contenido) {
+        for (int i = 1; i <= 10; i++) {
+            String nombrea = "archivo" + i + ".txt";
+            File archivo = new File(nombrea);
+
+            try {
+                PrintWriter salida = new PrintWriter(new FileWriter(archivo, true));
+                for (int j = 0; j < contenido.size(); j++) {
+                    String linea = contenido.get(j);
+                    salida.println(linea); // Cambiar a println para agregar saltos de línea
+                }
+                salida.close();
+
+                System.out.println("Se ha escrito en el archivo " + nombrea);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace(System.out);
+            }
+        }
     }
-}
 }
