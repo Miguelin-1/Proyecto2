9f3352bfb9aa4ba1e2e7eb524fd58696f792ce11
Hanna-gt <hgraciat@udistrital.edu.co>
2024-10-28 07:57:28 -0500

    adelante un poco de la interfaz nuvamente (diseño)

--- src/heroes_y_antiheroes.java
+++ src/heroes_y_antiheroes.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class heroes_y_antiheroes {
+    private JPanel marvel;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JPasswordField passwordField1;
+    private JButton GUARDARHEROEButton;
+    private JButton MODIFICARButton;
+    private JButton GUARDARANTIHEROEButton;
+    private JButton EXPORTARATXTButton;
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
