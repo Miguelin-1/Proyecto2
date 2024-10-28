ec235565aae515bf190b9d3a3938178bdfc3a2f3
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-27 23:39:06 -0500

    Pequeños cambios

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
         //ModificarArchivos.crearArchivos();
         Ventana v1= new Ventana();
         v1.setVisible(true);
-        Persona persona=new Persona(18,"Juan","Colombiano","Masculino","1.78","Soltero");
-        System.out.println(persona.toString());
+
     }
 
 
