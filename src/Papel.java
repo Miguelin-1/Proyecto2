5eca007d64ea842b144a8fc9a8d367978d0261b2
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-28 17:21:14 -0500

    Ya se decidió como realizar la interfaz

---
+++ src/Papel.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Papel extends Actor{
+private int anoInicio;
+private int anoFin;
+private ArrayList<String> peliculas = new ArrayList<>();
+
+    public Papel(int edad, String nombre, String nacionalidad, String genero, String altura, String estado_civil, String alias, int anosCarrera, ArrayList<String> premios, ArrayList<String> papeles, int anoInicio) {
+        super(edad, nombre, nacionalidad, genero, altura, estado_civil, alias, anosCarrera, premios, papeles);
+        this.anoInicio = anoInicio;
+        this.anoFin=anoFin;
+        this.peliculas=peliculas;
+    }
+
+    public int getAnoInicio() {
+        return anoInicio;
+    }
+
+    public void setAnoInicio(int anoInicio) {
+        this.anoInicio = anoInicio;
+    }
+
+    public int getAnoFin() {
+        return anoFin;
+    }
+
+    public void setAnoFin(int anoFin) {
+        this.anoFin = anoFin;
+    }
+
+    public ArrayList<String> getPeliculas() {
+        return peliculas;
+    }
+
+    public void setPeliculas(ArrayList<String> peliculas) {
+        this.peliculas = peliculas;
+    }
+
+    @Override
+    public String toString() {
+        return "Papel{" +
+                "anoInicio=" + anoInicio +
+                ", anoFin=" + anoFin +
+                ", peliculas=" + peliculas +
+                '}';
+    }
+}
