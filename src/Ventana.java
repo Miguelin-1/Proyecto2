eb68742d4fe546c78cf34f8c4941cda6c615ae3f
Miguelin-1 <jmbuitragog@udistrital.edu.co>
2024-10-28 23:16:04 -0500

    Avance en la Interfaz grafica

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Ventana extends JFrame {
 
@@ -8,22 +9,36 @@ public class Ventana extends JFrame {
         setLocationRelativeTo(null); //este metodo pone la ventana en el centro de la pantalla
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Creacion de héroes y antihéroes");
-        panelPrincipal();
+        panelP();
     }
-    private void panelPrincipal(){
-            JPanel panel=new JPanel();
-            this.getContentPane().add(panel);
-            JLabel etiqueta=new JLabel("Registro de héroes y antihéroes");
-            panel.add(etiqueta);
-            etiqueta.setSize(10,10);
-            etiqueta.setFont(new Font("arial",Font.PLAIN,20));
-        JLabel etiqueta1=new JLabel("¿Que quieres crear, héroe o anti héroe?");
-        panel.add(etiqueta1);
-        etiqueta.setSize(10,10);
-        etiqueta.setFont(new Font("arial",Font.PLAIN,20));
+    private void panelP(){
+            JPanel panelPrincipal=new JPanel();
+            panelPrincipal.setLayout(null);
+            this.getContentPane().add(panelPrincipal);
 
+        JLabel etiqueta = new JLabel("Registro de héroes y antihéroes");
+        etiqueta.setBounds(100, 20, 400, 30);
+        etiqueta.setFont(new Font("Times new roman", Font.BOLD, 20));
+        panelPrincipal.add(etiqueta);
+
+        JLabel etiqueta1 = new JLabel("¿Qué quieres crear, héroe o antihéroe?");
+        etiqueta1.setBounds(100, 60, 400, 30);
+        etiqueta1.setFont(new Font("Times new roman", Font.BOLD, 16));
+        panelPrincipal.add(etiqueta1);
+
+        JButton botonH = new JButton("Crear Héroe");
+
+        botonH.setBounds(25, 350, 125, 50);
+        panelPrincipal.add(botonH);
+
+
+        JButton botonAH = new JButton("Crear AntiHéroe");
+        botonAH.setBounds(175, 350, 125, 50);
+        panelPrincipal.add(botonAH);
+        JButton botonV = new JButton("Crear Villano");
+        botonV.setBounds(325, 350, 125, 50);
+        panelPrincipal.add(botonV);
 
     }
-
 
 }
